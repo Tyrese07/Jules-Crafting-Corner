e6b7bed2969b65750b87aff14a8a1f7e870147c1
Tyrese07 <93104450+Tyrese07@users.noreply.github.com>
2024-03-26 10:24:32 -0500

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,11 @@
+const config = {
+  cognito: {
+    region: 'us-east-2', // e.g., 'us-east-1'
+    cognito: {
+      userPoolId: 'us-east-2_QeclaepwX',
+      userPoolWebClientId: '5gc52bni7f1dtltgbun9r220rc',
+    },
+  },
+};
+
+module.exports = config;
