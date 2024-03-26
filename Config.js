c291923ec27cd90b3f6e64b30ee8ef558431d2cf
Tyrese07 <93104450+Tyrese07@users.noreply.github.com>
2024-03-26 13:18:51 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const config = {
   cognito: {
     region: 'us-east-2', // e.g., 'us-east-1'
     cognito: {
-      userPoolId: 'us-east-2_QeclaepwX',
-      userPoolWebClientId: '5gc52bni7f1dtltgbun9r220rc',
+      userPoolId: 'us-east-2_UIovAgpgj',
+      userPoolWebClientId: '4hbcdt8enhb8oud6tbd3ph6b58',
     },
     api: {
       invokeUrl: 'https://aielkezf1k.execute-api.us-east-2.amazonaws.com/Dev',
