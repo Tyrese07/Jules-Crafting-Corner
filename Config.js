43f57b4e5ec1435e9066ad815984f979e79c23b8
Tyrese07 <93104450+Tyrese07@users.noreply.github.com>
2024-03-26 12:36:29 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -5,6 +5,8 @@ const config = {
       userPoolId: 'us-east-2_QeclaepwX',
       userPoolWebClientId: '5gc52bni7f1dtltgbun9r220rc',
     },
+    api: {
+      invokeUrl: 'https://aielkezf1k.execute-api.us-east-2.amazonaws.com/Dev',
   },
 };
 
